3c5448224695f3cb5abfaee19d003a10808bda36
Noël Bossart <noel.bossart@namics.com>
2013-03-13 13:06:30 -0300

    Fixed JS issues

--- Configuration/Typo3/AdditionalConfiguration.php
+++ Configuration/Typo3/AdditionalConfiguration.php
@@ -1,30 +1,4 @@
 <?php        
-
-	
-	$GLOBALS['TYPO3_CONF_VARS']['BE']['fileadminDir'] = 'assets/';
-	$GLOBALS['TYPO3_CONF_VARS']['BE']['staticFileEditPath'] = 'static/';
-	$GLOBALS['TYPO3_CONF_VARS']['BE']['compressionLevel'] = '4';
-	$GLOBALS['TYPO3_CONF_VARS']['BE']['lockRootPath'] = $_SERVER['DOCUMENT_ROOT'];
-	$GLOBALS['TYPO3_CONF_VARS']['BE']['warning_email_addr'] =	$_SERVER['T3_WARNINGEMAILADDR'] ? $_SERVER['T3_WARNINGEMAILADDR'] : 'n.company@me.com';
-	$GLOBALS['TYPO3_CONF_VARS']['BE']['versionNumberInFilename'] = '1';
-	$GLOBALS['TYPO3_CONF_VARS']['BE']['allowDonateWindow'] = '0';
-
-	$GLOBALS['TYPO3_CONF_VARS']['FE']['additionalAbsRefPrefixDirectories'] = 'assets/,';
-	$GLOBALS['TYPO3_CONF_VARS']['FE']['compressionLevel'] = '6';
-
-	// Backend IP Restrictions and Warnings
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = $_SERVER['T3_DEVIPMASK'] ? $_SERVER['T3_DEVIPMASK'] : '127.0.0.1, 195.141.221.1, 192.168.0.*, 10.*'; // IP Mask for Backend
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['doNotCheckReferer'] = '1';
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['ddmmyy'] = 'd. m. Y';	 
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = '2'; 
-
-	$GLOBALS['TYPO3_CONF_VARS']['BE']['compressionLevel'] = '4';
-	$GLOBALS['TYPO3_CONF_VARS']['FE']['compressionLevel'] = '6';
-	
-	$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] = ',subtitle,description,keywords';
-	$GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields'] = 'uid,title,subtitle,nav_title,keywords,description,abstract,author,author_email,url,urltype,shortcut,shortcut_mode,tx_realurl_pathsegment';
-	$GLOBALS['TYPO3_CONF_VARS']['FE']['hidePagesIfNotTranslatedByDefault'] = '1';
-	
 	
 	$GLOBALS['TYPO3_CONF_VARS']['DB'] = array(
 		'database' => 't3_casaes',
@@ -33,6 +7,31 @@
 		'password' => '8cvb073!db',
 		'username' => 't3_casaes',
 	);
+
+	
+	$GLOBALS['TYPO3_CONF_VARS']['BE']['fileadminDir'] = 'assets/';
+	$GLOBALS['TYPO3_CONF_VARS']['BE']['staticFileEditPath'] = 'static/';
+	//$GLOBALS['TYPO3_CONF_VARS']['BE']['compressionLevel'] = '4';
+	$GLOBALS['TYPO3_CONF_VARS']['BE']['lockRootPath'] = $_SERVER['DOCUMENT_ROOT'];
+	$GLOBALS['TYPO3_CONF_VARS']['BE']['warning_email_addr'] =	$_SERVER['T3_WARNINGEMAILADDR'] ? $_SERVER['T3_WARNINGEMAILADDR'] : 'n.company@me.com';
+	//$GLOBALS['TYPO3_CONF_VARS']['BE']['versionNumberInFilename'] = '1';
+	$GLOBALS['TYPO3_CONF_VARS']['BE']['allowDonateWindow'] = '0';
+	$GLOBALS['TYPO3_CONF_VARS']['BE']['explicitADmode'] = '';
+	
+
+	$GLOBALS['TYPO3_CONF_VARS']['FE']['additionalAbsRefPrefixDirectories'] = 'assets/,';
+	//$GLOBALS['TYPO3_CONF_VARS']['FE']['compressionLevel'] = '6';
+
+	// Backend IP Restrictions and Warnings
+	$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = $_SERVER['T3_DEVIPMASK'] ? $_SERVER['T3_DEVIPMASK'] : '127.0.0.1, 195.141.221.1, 192.168.0.*, 10.*'; // IP Mask for Backend
+	$GLOBALS['TYPO3_CONF_VARS']['SYS']['doNotCheckReferer'] = '1';
+	$GLOBALS['TYPO3_CONF_VARS']['SYS']['ddmmyy'] = 'd. m. Y';	 
+	$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = '2';
+	
+	$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] = ',subtitle,description,keywords';
+	$GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields'] = 'uid,title,subtitle,nav_title,keywords,description,abstract,author,author_email,url,urltype,shortcut,shortcut_mode,tx_realurl_pathsegment';
+	$GLOBALS['TYPO3_CONF_VARS']['FE']['hidePagesIfNotTranslatedByDefault'] = '1';
+	
 
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = '0'; 
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['systemLog'] = '0';
@@ -60,143 +59,5 @@
 	$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['cooluri'] = 'a:3:{s:6:"LANGID";s:1:"L";s:7:"XMLPATH";s:44:"typo3conf/ext/nbobase/Configuration/Cooluri/";s:11:"MULTIDOMAIN";s:1:"0";}';
 	$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['rsaauth'] = 'a:1:{s:18:"temporaryDirectory";s:18:"/home/mondialw/tmp";}';
 	$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['saltedpasswords'] = 'a:2:{s:3:"FE.";a:2:{s:7:"enabled";s:1:"1";s:21:"saltedPWHashingMethod";s:28:"tx_saltedpasswords_salts_md5";}s:3:"BE.";a:2:{s:7:"enabled";s:1:"1";s:21:"saltedPWHashingMethod";s:28:"tx_saltedpasswords_salts_md5";}}';
-	
 
-
-	/* INSTRUCTIONS on how to use:
-	// Put the following lines in to the localconf.php
-	// localconf.php should only contain the extlists, all other settings are pasted to this file
-	// to allow releases
-	
-	$TYPO3_CONF_VARS['SYS']['encryptionKey'] = '…';	
-	$TYPO3_CONF_VARS['BE']['installToolPassword'] = '…';
-
-	$typo_db_username = 't3_instance';
-	$typo_db_password = 'password';
-	$typo_db_host = 'localhost';
-	$typo_db = 't3_instance';
-
-	// include this script
-	require_once(PATH_typo3conf.'ext/nbobase/Configuration/Localconf/default.php');
-
-	## INSTALL SCRIPT EDIT POINT TOKEN - all lines after this points may be changed by the install script!
-	// $TYPO3_CONF_VARS['EXT']['extList'] …
-	// $TYPO3_CONF_VARS['EXT']['extList_FE'] …
-	*/
-	
-	/*
-	// Base settings
-	$TYPO3_CONF_VARS['SYS']['sitename'] = 'bissFEST';
-
-	$TYPO3_CONF_VARS['SYS']['compat_version'] = '4.7';	
-	$TYPO3_CONF_VARS['INSTALL']['wizardDone']['tx_coreupdates_installsysexts'] = '1';	//  Modified or inserted by TYPO3 Upgrade Wizard.
-	$TYPO3_CONF_VARS['INSTALL']['wizardDone']['tx_coreupdates_installnewsysexts'] = '1';	//  Modified or inserted by TYPO3 Upgrade Wizard.
-	
-	// Needed to create Filemounts outside assets (for nmydummy resources mount)
-	$TYPO3_CONF_VARS['BE']['lockRootPath'] = $_SERVER['DOCUMENT_ROOT'];
-
-	// Force UTF-8 on System                
-	$TYPO3_CONF_VARS['SYS']['UTF8filesystem'] = '1';	  
-	$TYPO3_CONF_VARS['SYS']['setDBinit'] = 'SET NAMES utf8;\' . LF . \'SET CHARACTER_SET utf8;\' . LF . \'SET SESSION character_set_server=utf8;';	
-	$TYPO3_CONF_VARS['BE']['forceCharset'] = 'utf-8';
-		
-	// DB Settings
-	// see typo3conf/localconf.php
-
-	// Image settings
-	$TYPO3_CONF_VARS['GFX']['im_version_5'] = 'gm';
-	$TYPO3_CONF_VARS['GFX']['gdlib_png'] = '1'; 
-	$TYPO3_CONF_VARS['GFX']['im_combine_filename'] = 'combine'; 
-	$TYPO3_CONF_VARS['GFX']['im_path'] = '/usr/local/bin/'; 
-	$TYPO3_CONF_VARS['GFX']['im_path_lzw'] = '/usr/local/bin/';
-	$TYPO3_CONF_VARS['GFX']['thumbnails'] = '1';
-	$TYPO3_CONF_VARS['EXT']['allowLocalInstall'] = '0';
-	
-	
-	// Backend IP Restrictions and Warnings
-	$TYPO3_CONF_VARS['SYS']['devIPmask'] = $_SERVER['T3_DEVIPMASK'] ? $_SERVER['T3_DEVIPMASK'] : '127.0.0.1, 195.141.221.1, 192.168.0.*, 10.*'; // IP Mask for Backend
-	//$TYPO3_CONF_VARS['BE']['IPmaskList'] =	$_SERVER['T3_IPMASKLIST'] ? $_SERVER['T3_IPMASKLIST'] : '127.0.0.1, 195.141.221.1, 192.168.0.*, 10.*, 172.29.2.*'; // IP Mask for Backend
-	$TYPO3_CONF_VARS['BE']['warning_email_addr'] =	$_SERVER['T3_WARNINGEMAILADDR'] ? $_SERVER['T3_WARNINGEMAILADDR'] : 'n.company@me.com';
-	
-	// Display errors only if client matches [SYS][devIPmask]. 
-	// If devIPmask matches the users IP address the configured "debugExceptionHandler" is 
-	// used for exceptions, if not "productionExceptionHandler" will be used
-	
-	$TYPO3_CONF_VARS['BE']['compressionLevel'] = '4';
-	$TYPO3_CONF_VARS['FE']['compressionLevel'] = '6';
-	$TYPO3_CONF_VARS['BE']['loginSecurityLevel'] = 'normal';
-	
-	$TYPO3_CONF_VARS['SYS']['displayErrors'] = '2'; 
-	$TYPO3_CONF_VARS['SYS']['systemLog'] = '1';
-	$TYPO3_CONF_VARS['SYS']['enableDeprecationLog'] = '1';
-	$TYPO3_CONF_VARS['SYS']['enable_DLOG'] = '1';
-	
-	if($_SERVER['ENVIRONMENT'] == 'local'){
-		$TYPO3_CONF_VARS['SYS']['systemLog'] = '1';
-		$TYPO3_CONF_VARS['SYS']['enableDeprecationLog'] = '1';
-		$TYPO3_CONF_VARS['SYS']['enable_DLOG'] = '1';
-
-		$TYPO3_CONF_VARS['EXT']['allowLocalInstall'] = '1';
-
-		$TYPO3_CONF_VARS['GFX']['im_path'] = '/usr/local/bin/';	
-		$TYPO3_CONF_VARS['GFX']['im_path_lzw'] = '/usr/local/bin/';
-		$TYPO3_CONF_VARS['GFX']['im_combine_filename'] = 'composite';
-		$TYPO3_CONF_VARS['GFX']['im_version_5'] = 'im6';
-
-		$TYPO3_CONF_VARS['BE']['compressionLevel'] = '';
-		$TYPO3_CONF_VARS['FE']['compressionLevel'] = '';
-	} else {
-		$TYPO3_CONF_VARS['SYS']['displayErrors'] = '0'; 
-		$TYPO3_CONF_VARS['SYS']['systemLog'] = '0';
-		$TYPO3_CONF_VARS['SYS']['enableDeprecationLog'] = '0';
-		$TYPO3_CONF_VARS['SYS']['enable_DLOG'] = '0';
-	}
-
-	// New Fileadmin
-	$TYPO3_CONF_VARS['BE']['fileadminDir'] = 'assets/';
-	$TYPO3_CONF_VARS['BE']['staticFileEditPath'] = 'assets/static/';
-	$TYPO3_CONF_VARS['FE']['additionalAbsRefPrefixDirectories'] = 'assets/,';
-
-	// Performance
-	$TYPO3_CONF_VARS['SYS']['no_pconnect'] = '1';
-
-
-	
-	// Firefox doesn't work: 
-	$TYPO3_CONF_VARS['SYS']['doNotCheckReferer'] = '1';
-	
-	// Security
-	$TYPO3_CONF_VARS['BE']['disable_exec_function'] = '1'; 
-	$TYPO3_CONF_VARS['BE']['usePHPFileFunctions'] = '1';
-	$TYPO3_CONF_VARS['BE']['noPHPscriptInclude'] = '1';
-	$TYPO3_CONF_VARS['SYS']['cookieHttpOnly'] = '1';
-	$TYPO3_CONF_VARS['SYS']['cookieSecure'] = '2';	
-	$TYPO3_CONF_VARS['BE']['explicitADmode'] = 'explicitDeny';
-
-	// Date Settings
-	$TYPO3_CONF_VARS['SYS']['ddmmyy'] = 'd. m. Y';	 
-
-	// Disable Backend Donate Window
-	$TYPO3_CONF_VARS['BE']['allowDonateWindow'] = '0';
-	//$TYPO3_CONF_VARS['BE']['interfaces'] = 'backend,frontend';
-	$TYPO3_CONF_VARS['BE']['interfaces'] = 'backend';
-	// Modyfie Backend layout to display checkboxes in Access-Lists (BE-Usergroup Config)
-	// $TYPO3_CONF_VARS['BE']['accessListRenderMode'] = 'checkbox';
-	
-	//$TYPO3_CONF_VARS['BE']['fileDenyPattern'] = '\\.(php[3-6]?|phpsh|phtml)(\\..*)?$|^\\.htaccess$';
-	$TYPO3_CONF_VARS['BE']['versionNumberInFilename'] = '0';
-
-	// addRootLineFields
-	//$TYPO3_CONF_VARS['FE']['addRootLineFields'] = ',subtitle,tx_templavoila_ds,tx_templavoila_to,tx_templavoila_next_ds,tx_templavoila_next_to,tx_realurl_pathsegment,tx_realurl_exclude,tx_realurl_pathoverride,description,keywords';
-	//$TYPO3_CONF_VARS['FE']['pageOverlayFields'] = 'uid,title,subtitle,nav_title,keywords,description,abstract,author,author_email,url,urltype,shortcut,shortcut_mode,tx_realurl_pathsegment';
-
-	// Extension Settings
-	$TYPO3_CONF_VARS['EXT']['extConf']['em'] = 'a:1:{s:17:"selectedLanguages";s:2:"de";}';	//  Modified or inserted by TYPO3 Extension Manager.
-	$TYPO3_CONF_VARS['EXT']['extConf']['dce'] = 'a:2:{s:7:"DEVMODE";s:1:"1";s:16:"ENABLECODEMIRROR";s:1:"1";}';	// Modified or inserted by TYPO3 Extension Manager. 
-	$TYPO3_CONF_VARS['EXT']['extConf']['extension_builder'] = 'a:3:{s:15:"enableRoundtrip";s:1:"1";s:15:"backupExtension";s:1:"1";s:9:"backupDir";s:35:"uploads/tx_extensionbuilder/backups";}';	//  Modified or inserted by TYPO3 Extension Manager.
-	$TYPO3_CONF_VARS['EXT']['extConf']['cooluri'] = 'a:3:{s:6:"LANGID";s:1:"L";s:7:"XMLPATH";s:44:"typo3conf/ext/nbobase/Configuration/Cooluri/";s:11:"MULTIDOMAIN";s:1:"0";}';	//  Modified or inserted by TYPO3 Extension Manager.
-	$TYPO3_CONF_VARS['EXT']['extConf']['rsaauth'] = 'a:1:{s:18:"temporaryDirectory";s:18:"/home/mondialw/tmp";}';	// Modified or inserted by TYPO3 Extension Manager. 
-	$TYPO3_CONF_VARS['EXT']['extConf']['saltedpasswords'] = 'a:2:{s:3:"FE.";a:2:{s:7:"enabled";s:1:"1";s:21:"saltedPWHashingMethod";s:28:"tx_saltedpasswords_salts_md5";}s:3:"BE.";a:2:{s:7:"enabled";s:1:"1";s:21:"saltedPWHashingMethod";s:28:"tx_saltedpasswords_salts_md5";}}';	//  Modified or inserted by TYPO3 Extension Manager.	
-	
-	/**/
 ?>
--- Resources/Public/js/application.js
+++ Resources/Public/js/application.js
@@ -4,6 +4,48 @@
 ;
 (function($){
 	$(document).ready(function(){
+		// google maps
+		$('.gmap').each(function(){
+			var that = this;
+			var $t = $(this);
+			var lat = $t.attr('data-lat') * 1
+			var lng = $t.attr('data-lng') * 1
+
+			var myOptions = {
+				zoom: 6,
+				center: new google.maps.LatLng(lat, lng), // Bissfest
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			}
+
+			var search = {
+				'address': $t.attr('data-address').replace(/\r?\n|\r/g, ", "),
+				region: 'sgg'
+			};
+
+			var address = $t.html();
+			var map = new google.maps.Map(that, myOptions);
+			var gc  = geocoder = new google.maps.Geocoder();
+			var infowindow = new google.maps.InfoWindow();
+			var marker;
+
+			gc.geocode( search , function(results, status) {
+				if (status == google.maps.GeocoderStatus.OK) {
+					marker = new google.maps.Marker({
+						map: map,
+						position: results[0].geometry.location
+					});
+					map.setZoom($t.attr('data-zoom')*1);
+					map.setCenter(results[0].geometry.location);
+					if($t.attr('data-infowindow')){
+						infowindow.setContent(address);
+						infowindow.open(map, marker);
+					}
+				} else {
+					//console.log([results,status]);
+				}
+			});
+		});
+		
 		$('.fancybox').fancybox({
 			prevEffect : 'none',
 			nextEffect : 'none',
