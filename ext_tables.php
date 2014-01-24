e4d72836239e68aee7ab7a6a8f7ee0fa83f11ed5
Noel Bossart <noel.bossart@namics.com>
2014-01-24 16:40:57 +0100

    Added CSS and new backend config
    Unified NewsList and RSS Reaser

--- ext_tables.php
+++ ext_tables.php
@@ -15,4 +15,7 @@ require_once(t3lib_extMgm::extPath($_EXTKEY).'incl/typoscript.php');
 // Includes ClearCache Menu Entry
 // require_once(t3lib_extMgm::extPath($_EXTKEY).'incl/clearcache.php');
 
+if (TYPO3_MODE == 'BE') {
+	$GLOBALS['TBE_STYLES']['inDocStyles_TBEstyle'] = file_get_contents(t3lib_extMgm::extPath($_EXTKEY) . 'Resources/Public/css/be.css');
+}
 ?>
